2d57529bf031a200092cdb9aaaf378a46e11cef1
Alex Cooksey <aecooksey2651@gmail.com>
2022-06-09 11:08:35 -0400

    Completed assignment

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,35 @@
 function romanNumeral(string) {
-  // type your code here
+  const romans = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
+  };
+  let total = 0;
+  let idx = 0;
+
+  while (idx < string.length) {
+    const twoChar = string[idx] + (string[idx + 1] || '');
+
+    if (romans[twoChar] !== undefined) {
+      total += romans[twoChar];
+      idx += 2;
+    } else {
+      total += romans[string[idx]];
+      ++idx;
+    }
+  }
+
+  return total;
 }
 
 if (require.main === module) {
@@ -22,3 +52,9 @@ module.exports = romanNumeral;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+// I, II, III, IV, V, VI, VII, VIII, IX, X
+// XX, XXX, XL, L, LX, LXX, LXXX, XC, C
+// CC, CCC, CD, D, DC, DCC, DCCC, CM
+// If smaller symbol after larger symbol, add them. If smaller symbol before larger, subtract
+// Don't use same number more than 3x in a row
